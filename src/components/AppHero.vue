ca59893d1567acbbf06ba3909a80d7cdc0333e92
marco <lazzari.marco.ml@gmail.com>
2024-02-28 14:02:19 +0100

    aggiunto titolo e immagine con componenti; esercizio finito

---
+++ src/components/AppHero.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="img-wrapper">
+        <img src="https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg" alt="">
+    </div>
+</template>
+
+<style>
+    .img-wrapper{
+        width: 800px;
+        height: 800px;
+
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        transform: translateX(-50%);
+    }
+
+    .img-wrapper img{
+        width: 100%;
+    }
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>La mia prima app con Vite!</h1>
+</template>
+
+<style scoped>
+    h1{
+        text-align: center;
+        padding: 2rem;
+        font-size: 3rem;
+    }
+</style>
